34a807fd7de3e98614ffdeaaa47be6bfaf2cd2e7
dekasins <dekasins@gmail.com>
2020-02-29 17:45:06 +0300

    feat: add function max()

--- src/index.js
+++ src/index.js
@@ -32,7 +32,34 @@ exports.min = function min (array) {
 }
 
 exports.max = function max (array) {
-  return 0;
+  let m;
+  if (Array.isArray(array) && array.length > 0){
+     console.log ("yes" );
+ 
+    for (let i = 0; i<array.length; i++) {
+  
+      if (Number.isInteger(array[i])){
+        m = array[i];
+       break;
+       }
+    }   
+      if (m == undefined) { 
+       m = 0;   
+      }else{
+          
+        for (let i = 0; i<array.length; i++){
+         
+       if (Number.isInteger(array[i]) && array[i] > m) {
+         m = array[i];
+         }
+       }
+  
+    } 
+
+  }else{
+   m = 0; 
+  }
+   return m;
 }
 
 exports.avg = function avg (array) {
