1ffced531626573cc2aebfdb17ac8ffe31bc2687
dekasins <dekasins@gmail.com>
2020-02-29 17:38:54 +0300

    feat: add function min()

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 
 exports.min = function min (array) {
-  return 0;
+  
+    let m;
+ if (Array.isArray(array) && array.length > 0){
+    console.log ("yes" );
+
+   for (let i = 0; i<array.length; i++) {
+ 
+     if (Number.isInteger(array[i])){
+       m = array[i];
+      break;
+      }
+   }   
+     if (m == undefined) { 
+      m = 0;   
+     }else{
+         
+       for (let i = 0; i<array.length; i++){
+        
+      if (Number.isInteger(array[i]) && array[i] < m) {
+        m = array[i];
+        }
+      }
+ 
+   } 
+
+ }else{
+  m = 0; 
+ }
+  return m;
 }
 
 exports.max = function max (array) {
